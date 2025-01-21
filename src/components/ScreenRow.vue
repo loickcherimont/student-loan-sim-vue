4280fa2b559c084f199d2ed3afcd6fc36b81eec0
loickcherimont <loickcherimont@gmail.com>
2025-01-21 22:03:36 +0100

    Add propertyType prop to display %, € or nothing in function of the type of data

--- src/components/ScreenRow.vue
+++ src/components/ScreenRow.vue
@@ -2,12 +2,21 @@
 defineProps<{
     property: string
     propertyValue: string | number
+    propertyType?: "rate" | "money"
 }>()
 </script>
 
 <template>
-    <li class="border-b last:border-0 border-slate-600 flex justify-between p-5 w-72">
+    <li v-if="propertyType === 'rate'" class="border-b last:border-0 border-slate-600 flex justify-between p-5 w-72">
         <span>{{ property }}</span>
-        <span>{{ propertyValue || 0 }}</span>
+        <span>{{ propertyValue }} %</span>
+    </li>
+    <li v-else-if="propertyType === 'money'" class="border-b last:border-0 border-slate-600 flex justify-between p-5 w-72">
+        <span>{{ property }}</span>
+        <span>{{ propertyValue }} €</span>
+    </li>
+    <li v-else class="border-b last:border-0 border-slate-600 flex justify-between p-5 w-72">
+        <span>{{ property }}</span>
+        <span>{{ propertyValue }}</span>
     </li>
 </template>
